bbc25218ebce4a25fa9d821794c9d26eccc0f1e2
nerea <nerecandia24@gmail.com>
2024-09-09 11:31:52 -0300

    avance con las clases del juego

---
+++ Apuesta.py
@@ -0,0 +1,12 @@
+class apuesta :
+    def __init__(self):
+        self.fichas= 0
+    def __repr__(seld):
+        return f"Apuesta:{self.fichas}fichas"
+    def PonerFichas(self, cuantas=1):
+        slef.fichas = self.fichas + 4
+    def tomarfichas(slef, cuasntas=1):
+        if cuantas > self.fichas:
+           raise ValueError("No hay tantas fichas ()")
+        self.fichas = self.fichas - cuantas 
+  
---
+++ Perinola/test_apuesta.py
@@ -0,0 +1,20 @@
+from apuesta import Apuesta
+
+def test_prueba():
+     assert(True)
+
+def tets_constructor(): test_Apuesata.py::test_constructor fail
+assert = Apuesta()
+assert(a.fichas == 0)
+    
+def test_repr():
+        assert = Apuesta()
+        msj = a.__repr__()
+        assert("Apuesta" in msj)
+        assert("fichas" in msj)
+        assert("4" in msj)
+
+def test_ponerFichas():
+        a = Apuesta()
+        a.ponerFicha(4)
+        assert(a.fichas == 4)
